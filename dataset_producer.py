89d8ec1c304048877a0d7533ff7f685cac0cb92e
skybig233 <296392014@qq.com>
2022-01-17 18:22:43 +0800

    兼容有标签、有头部数据集

--- dataset_producer.py
+++ dataset_producer.py
@@ -8,16 +8,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.datasets import *
-
+from collections import Counter
 FIG_SIZE=(6,6)
 class Dataset:
 
-    def __init__(self,path='',data:np.ndarray=None):
+    def __init__(self, path='', data:np.ndarray=None, isLabeled=False, haveHead=False):
         self.path=path
         self.data=data
+        #load则label是列表，随机生成则label是np.ndarray
+        self.label=[]
 
+        self.head=[]
         if self.path!='':
-            self.load_data()
+            self.load_data(isLabeled=isLabeled,haveHead=haveHead)
 
     @property
     def size(self):
@@ -25,22 +28,35 @@ class Dataset:
     @property
     def dimension(self):
         return self.data.shape[1]
+    @property
+    def label_count(self):
+        return Counter(self.label)
 
-    # def load_data(self):
-    #     self.data=pd.read_csv(self.path)
-
-    def load_data(self)->np.array:
+    def load_data(self, isLabeled=False,haveHead=False)->np.array:
         """
-        将txt文件中的数据转换成np数组
+        将txt文件中的数据转换成np数组，存入data属性中，并返回data属性值
         :param filename:模拟数据文件路径
         :return:np数组
         """
-        dataset = []
+
         with open(self.path) as file:
+            first_line=file.readline()
+            spliter=check_spliter(first_line)
+
+        with open(self.path) as file:
+            if haveHead:
+                first_line = file.readline()
+                self.head=first_line.strip().split(spliter)
+
+            dataset = []
             for line in file.readlines():
-                lineArr = line.strip().split('\t')
-                m = len(lineArr)
-                dataset.append(lineArr[0:m])
+                lineArr = line.strip().split(spliter)
+                if isLabeled:
+                    # 如果有标签的数据集，标签一般在最后一列
+                    # 对每个标签统计个数，并用数字0123代替该标签字符串
+                    self.label.append(lineArr[-1])
+                    lineArr=lineArr[:-1]
+                dataset.append(lineArr)
             self.data = np.array(dataset, dtype=np.float64)
             # print("该数据集有%d个%d维数据" % (data.shape[0], data.shape[1]))
         return self.data
@@ -49,7 +65,11 @@ class Dataset:
         """
         将numpy_ndarray输出到txt文件
         """
-        np.savetxt(self.path,self.data,delimiter='\t')
+        if self.label==[]:
+            np.savetxt(self.path,self.data,delimiter='\t')
+        else:
+            np.savetxt(self.path, np.column_stack((self.data, self.label)), delimiter='\t',
+                       fmt=['%f'] * self.dimension + ['%d'])
         return
 
     def normalize_data(self,dimension=[]):
@@ -73,17 +93,52 @@ class Dataset:
     def show_shape(self):
         print ("该数据集有%d个%d维数据"%(self.size,self.dimension))
 
-    def draw_data(self):
+    def draw_2D_data(self,x=0,y=1):
         """
         用matplotlib绘制数据
+        :param x:x轴默认是第0列的数据
+        :param y:y轴默认是第1列的数据
         """
         self.show_shape()
-        # 可视化一下
-        plt.scatter(self.data[:, 0], self.data[:, 1], marker='o', color='r')
-        plt.xlabel('x0')
-        plt.ylabel('x1')
+        assert x<self.dimension and y<self.dimension
+
+        if self.label==[]:
+            # 无标签数据集可视化
+            # 可视化一下
+            plt.scatter(self.data[:, x], self.data[:, y], marker='o', color='r')
+        else:
+            # 有标签数据集可视化
+            # 每个标签scatter一次，按label_count中label的顺序（即最多标签的优先scatter）
+            new_label=self.label_to_nparray()
+            for i in range(len(self.label_count)):
+                plt.scatter(self.data[new_label==i][:, x],self.data[new_label==i][:, y],marker='o')
+
+            #设置图例
+            legend_title=self.head[-1] if self.head else None
+            plt.legend(self.label_count.keys(),title=legend_title)
+
+        # 如果数据集有头，那么xy轴就有意义，绘制时要添加说明
+        if self.head!=[]:
+            plt.xlabel(self.head[x])
+            plt.ylabel(self.head[y])
+        else:
+            plt.xlabel('x0')
+            plt.ylabel('x1')
         plt.show()
 
+    def label_to_nparray(self)->np.ndarray:
+        """
+        标签数字化，用np.array代替label字符串标签
+        :return:np.array
+        """
+        if isinstance(self.label,np.ndarray):
+            return self.label
+
+        ans=[]
+        list_label_int = list(self.label_count.keys())
+        for i in self.label:
+            ans.append(list_label_int.index(i))
+        return np.array(ans)
 
 def rand_uniform_data(n:int, d:int)->Dataset:
     """
@@ -100,18 +155,21 @@ def rand_moon_data(n:int)->Dataset:
     new_set = Dataset()
     x1,y1=make_moons(n_samples=n,noise=0.07,random_state=120)
     new_set.data=x1
+    new_set.label=y1
     return new_set
 
 def rand_circles_data(n:int)->Dataset:
     new_set = Dataset()
     x1,y1=make_circles(n_samples=n,noise=0.07,random_state=16,factor=0.5)
     new_set.data=x1
+    new_set.label=y1
     return new_set
 
 def rand_blob_data(n:int)->Dataset:
     new_set = Dataset()
     x1,y1=make_blobs(n_samples=n,n_features=2,centers=3,random_state=170)
     new_set.data=x1
+    new_set.label=y1
     return new_set
 
 def rand_bars_data(n:int)->Dataset:
@@ -120,8 +178,17 @@ def rand_bars_data(n:int)->Dataset:
     transformation = [[0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
     x1 = np.dot(x1, transformation)
     new_set.data=x1
+    new_set.label=y1
     return new_set
 
+
+def check_spliter(line: str) -> str:
+    SPLITER = ['\t', ',']
+    for i in SPLITER:
+        lineArr = line.strip().split(i)
+        if len(lineArr) > 1:
+            return i
+    return ''
 # def pic_data(picpath:str)->Dataset:
 #     # TODO 图片转换成模拟的二维数据集
 #     #  给定任意图片，先转换成灰度图，
@@ -148,10 +215,15 @@ def main():
     # a.write_data()
     # a.show_data()
 
-    path='kmeans/testdata/circlesSet.txt'
-    new_set=rand_circles_data(300)
+    path='knn_code/testdata/moonSet.txt'
+    new_set=rand_moon_data(300)
     new_set.path=path
+    new_set.draw_2D_data()
     new_set.write_data()
+
+    # path='knn_code/testdata/IRIS.csv'
+    # new_dataset=Dataset(path=path,isLabeled=True,haveHead=True)
+    # new_dataset.draw_2D_data(x=1,y=2)
 
 if __name__ == '__main__':
     main()
