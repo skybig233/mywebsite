46ed1965e8e6f515de352448efd256f7075fe425
skybig233 <296392014@qq.com>
2022-02-18 18:31:10 +0800

    kmeans改版，用pandas展示 dataset改用pandas存储 修改对应dataset逻辑，加入对dataset的自动判断以及字符串处理

--- dataset_producer.py
+++ dataset_producer.py
@@ -4,28 +4,30 @@
 # @File    : dataset_producer.py
 # @Software: PyCharm
 # @Description: 1、定义数据集类型2、使用sklearn.datasets生成模拟数据集
-from typing import Union
+from typing import Union, Dict, Any, Optional
 
+import pandas as pd
 import matplotlib.figure
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy import ndarray
 from sklearn.datasets import *
-from collections import Counter
+
+from number import isNumber
 FIG_SIZE=(6,6)
 class Dataset:
+    # 以IRIS数据集为例，第4列species是离散型字符串，则该字典中存储{4:{Iris-setosa:0,Iris-versicolor:1,Iris-virginica:2}}
+    # 即每个离散的类型取值用一个数字表示
+    count_dict: Dict[int, pd.Series]
 
-    data: Union[ndarray, ndarray, None]
-
-    def __init__(self, path='', data:np.ndarray=None, isLabeled=False, haveHead=False):
+    def __init__(self, path='', data:pd.DataFrame=None, haveHead:bool=None):
         self.path=path
         self.data=data
         #load则label是列表，随机生成则label是np.ndarray
-        self.label=[]
+        self.count_dict={}
 
         self.head=[]
         if self.path!='':
-            self.load_data(isLabeled=isLabeled,haveHead=haveHead)
+            self.load_data(haveHead=haveHead)
 
     @property
     def size(self):
@@ -34,48 +36,41 @@ class Dataset:
     def dimension(self):
         return self.data.shape[1]
     @property
-    def label_count(self):
-        return Counter(self.label)
-
-    def load_data(self, isLabeled=False,haveHead=False)->np.array:
+    def numpydata(self):
+        return self.data.to_numpy()
+    def load_data(self,haveHead=False):
         """
         将txt文件中的数据转换成np数组，存入data属性中，并返回data属性值
         :param filename:模拟数据文件路径
         :return:np数组
         """
+        #预读取加载信息
+        spliter,haveHead,isstr=self.check_head()
 
         with open(self.path) as file:
-            first_line=file.readline()
-            spliter=check_spliter(first_line)
-
-        with open(self.path) as file:
-            if haveHead:
-                first_line = file.readline()
-                self.head=first_line.strip().split(spliter)
-
             dataset = []
+            if haveHead:
+                file.readline()
             for line in file.readlines():
                 lineArr = line.strip().split(spliter)
-                if isLabeled:
-                    # 如果有标签的数据集，标签一般在最后一列
-                    # 对每个标签统计个数，并用数字0123代替该标签字符串
-                    self.label.append(lineArr[-1])
-                    lineArr=lineArr[:-1]
                 dataset.append(lineArr)
-            self.data = np.array(dataset, dtype=np.float64)
-            # print("该数据集有%d个%d维数据" % (data.shape[0], data.shape[1]))
-        return self.data
+            self.data=pd.DataFrame(data=dataset,columns=self.head)
+            self.data=self.data.astype(isstr)
+
+    def preprocess_data(self):
+        for i in range(self.dimension):
+            if self.data.dtypes[i]=='object':
+                property_name=self.data.columns[i]
+                clst: pd.Series=self.data[property_name].value_counts()
+                self.count_dict[i]=clst
+                replace_dict={clst.index[i]:i for i in range(len(clst))}
+                self.data[property_name]=self.data[property_name].replace(replace_dict)
 
     def write_data(self):
         """
         将numpy_ndarray输出到txt文件
         """
-        if self.label==[]:
-            np.savetxt(self.path,self.data,delimiter='\t')
-        else:
-            np.savetxt(self.path, np.column_stack((self.data, self.label)), delimiter='\t',
-                       fmt=['%f'] * self.dimension + ['%d'])
-        return
+        np.savetxt(self.path,self.data.to_numpy(),delimiter='\t')
 
     def normalize_data(self,dimension=[]):
         """
@@ -89,64 +84,81 @@ class Dataset:
             self.data[:,i]=(self.data[:,i] - np.min(self.data[:,i]))/_range
         return
 
-    def show_data(self):
-        self.show_shape()
-        with open(self.path,mode='r') as f:
-            for line in f:
-                print(line)
-
     def show_shape(self):
         print ("该数据集有%d个%d维数据"%(self.size,self.dimension))
 
-    def draw_2D_data(self,x=0,y=1)->(plt.figure,plt.axis):
+    def draw_2D_data(self,x=0,y=1,label:int=None)->(plt.figure,plt.axis):
         """
         用matplotlib绘制数据
-        :param x:x轴默认是第0列的数据
-        :param y:y轴默认是第1列的数据
+        :param x:作为x轴的列，默认是第0列的数据
+        :param y:作为y轴的列，默认是第1列的数据
+        :param label:分类标签列，可以是int、bool、None，False和None代表无标签，True默认最后一列
         """
         self.show_shape()
         assert x<self.dimension and y<self.dimension
         fig: matplotlib.figure.Figure=plt.figure()
         ax: matplotlib.figure.Axes = fig.add_subplot(111)
-        if self.label==[]:
-            # 无标签数据集可视化
+
+        pltdata=self.data.to_numpy()
+
+        if label==True:
+            label=self.dimension-1
+        if label==None or label==False:
+            # 无标签数据集
             # 可视化一下
-            ax.scatter(self.data[:, x], self.data[:, y], marker='x', color='blue')
+            ax.scatter(pltdata[:, x], pltdata[:, y], marker='x', color='blue')
         else:
             # 有标签数据集可视化
-            # 每个标签scatter一次，按label_count中label的顺序（即最多标签的优先scatter）
-            new_label=self.label_to_nparray()
-            for i in range(len(self.label_count)):
-                ax.scatter(self.data[new_label==i][:, x],self.data[new_label==i][:, y],marker='o')
-
+            for i in range(len(self.count_dict[label])):
+                #在数据集的label列，选出类型为i的，第xy列，作为xy
+                pltx=pltdata[pltdata[:, label] == i][:, x]
+                plty=pltdata[pltdata[:,label]==i][:, y]
+                ax.scatter(pltx,plty,marker='o')
             #设置图例
             legend_title=self.head[-1] if self.head else None
-            ax.legend(self.label_count.keys(),title=legend_title)
-
-        # 如果数据集有头，那么xy轴就有意义，绘制时要添加说明
-        if self.head!=[]:
-            ax.set_xlabel(self.head[x])
-            ax.set_ylabel(self.head[y])
-        else:
-            ax.set_xlabel('x0')
-            ax.set_ylabel('x1')
+            ax.legend(self.count_dict[label].keys(), title=legend_title)
+        #设置坐标轴
+        ax.set_xlabel(self.head[x])
+        ax.set_ylabel(self.head[y])
         return fig,ax
-        # plt.show()
 
-    def label_to_nparray(self)->np.ndarray:
+    def check_head(self)->(str,bool,[]):
         """
-        标签数字化，用np.array代替label字符串标签
-        :return:np.array
+        读取数据集的两行，可以确定：数据集的分隔符、是否有头、每个属性的名字和数据类型
+        :return:分隔符、是否有头、数据类型列表
         """
-        if isinstance(self.label,np.ndarray):
-            return self.label
+        SPLITER = ['\t', ',']#候选分隔符集合
+        #返回值初始化
+        spliter=''
+        headflag=False
+        isstr={}#记录每个属性的数据类型，目前只判断有效数字和字符串 TODO：增加判断类型
 
-        ans=[]
-        list_label_int = list(self.label_count.keys())
-        for i in self.label:
-            ans.append(list_label_int.index(i))
-        return np.array(ans)
+        with open(self.path) as file:
+            #首先确定分隔符
+            first,second=file.readline(),file.readline()
+            for i in SPLITER:
+                lineArr = first.strip().split(i)
+                if len(lineArr) > 1:
+                    spliter=i
+                    break
+            #判断完毕直接切分
+            first,second = first.strip().split(spliter),second.strip().split(spliter)
+            #然后确定数据集是否有头
+            for i in range(len(first)):
+                if isNumber(first[i])!=isNumber(second[i]):
+                    headflag=True
+                    break
+            self.head=first if headflag else ['X%d'%i for i in range(len(first))]
+            #最后确定每个属性的数据类型
+            for i in range(len(second)):
+                if isNumber(second[i]):
+                    isstr[self.head[i]]='float32'
+                else:
+                    isstr[self.head[i]] = 'object'
+                    self.count_dict[i]={}
+        return spliter,headflag,isstr
 
+TMP_FILE='./tempSet.txt'
 def rand_uniform_data(n:int, d:int)->Dataset:
     """
     随机生成数据
@@ -154,8 +166,9 @@ def rand_uniform_data(n:int, d:int)->Dataset:
     :param d: 维度
     :return:
     """
-    new_set=Dataset()
-    new_set.data=np.random.rand(n,d)
+    x=np.random.rand(n, d)
+    np.savetxt(TMP_FILE,x,delimiter='\t')
+    new_set=Dataset(path=TMP_FILE)
     return new_set
 
 def rand_moon_data(n:int)->Dataset:
@@ -189,13 +202,7 @@ def rand_bars_data(n:int)->Dataset:
     return new_set
 
 
-def check_spliter(line: str) -> str:
-    SPLITER = ['\t', ',']
-    for i in SPLITER:
-        lineArr = line.strip().split(i)
-        if len(lineArr) > 1:
-            return i
-    return ''
+
 # def pic_data(picpath:str)->Dataset:
 #     # TODO 图片转换成模拟的二维数据集
 #     #  给定任意图片，先转换成灰度图，
@@ -216,14 +223,20 @@ def main():
     """
     测试代码
     """
-    # a=Dataset(path='kmeans/testdata/testSet.txt')
+    # a=Dataset(path='knn_code/testdata/IRIS.csv')
+    # a = Dataset(path='kmeans/testdata/barsSet.txt')
+    a=rand_uniform_data(300,2)
+    a.preprocess_data()
+    a.draw_2D_data()
+    plt.show()
+    # a.show_data()
     # a.load_data()
     # a.normalize_data()
     # a.write_data()
     # a.show_data()
 
     # path='knn_code/testdata/moonSet.txt'
-    # new_dataset = Dataset(path=path,isLabeled=True)
+    # new_dataset = Dataset(path=path,label=True)
     # rand_data = np.zeros((1,new_dataset.dimension))
     # for i in range(new_dataset.dimension):
     #     low = np.min(new_dataset.data[:, i])
